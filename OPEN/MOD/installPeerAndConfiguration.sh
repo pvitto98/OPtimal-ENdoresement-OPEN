cc03fc464a40137f98b5f989c087bb191e9bcbdb
pvitto98 <62291477+pvitto98@users.noreply.github.com>
2022-03-10 10:30:06 +0100

    Changed peer binary version

--- OPEN/MOD/installPeerAndConfiguration.sh
+++ OPEN/MOD/installPeerAndConfiguration.sh
@@ -6,9 +6,9 @@
 #
 
 # if version not passed in, default to latest released version
-VERSION=2.4.2
+VERSION=2.2.2
 # if ca version not passed in, default to latest released version
-CA_VERSION=1.5.2
+CA_VERSION=1.4.9
 ARCH=$(echo "$(uname -s|tr '[:upper:]' '[:lower:]'|sed 's/mingw64_nt.*/windows/')-$(uname -m |sed 's/x86_64/amd64/g')" |sed 's/darwin-arm64/darwin-amd64/g')
 MARCH=$(uname -m)
 
